f5a333b73684f7230d60eee2f2c1f94a97958561
Ihdir <ssmmworld1@gmail.com>
2024-09-07 11:09:23 +0530

    errors

--- script.js
+++ script.js
@@ -5,20 +5,19 @@ const scroll = new LocomotiveScroll({
 });
 
 
-var elemC = document.querySelector("#elem-cointainer")
-var fixed = document.querySelector("#fixed-image")
-elemC.addEventListener("mouseenter", function() {
-    fixed.style.display = "block"
-})
-elemC.addEventListener("mouseleave", function() {
-    fixed.style.display = "none"
-})
-
-var elems = document.querySelectorAll(".elem")
-elems.forEach(function(e){
-    console.log("hey")
-    e.addEventListener("mouseenter", function(){
-        var image = e.getAttribute("data-image")
-        fixed.style.backgroundImage = 'url(${image})'
+var elemC = document.querySelector("#elem-container")
+    var fixed = document.querySelector("#fixed-image")
+    elemC.addEventListener("mouseenter", function () {
+        fixed.style.display = "block"
     })
-})
+    elemC.addEventListener("mouseleave", function () {
+        fixed.style.display = "none"
+    })
+
+    var elems = document.querySelectorAll(".elem")
+    elems.forEach(function (e) {
+        e.addEventListener("mouseenter", function () {
+            var image = e.getAttribute("data-image")
+            fixed.style.backgroundImage = `url(${image})`
+        })
+    })
